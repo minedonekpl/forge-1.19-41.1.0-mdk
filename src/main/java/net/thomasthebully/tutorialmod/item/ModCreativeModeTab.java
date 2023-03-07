292b82863d64b5f9157c849ea0572aef94490559
minedonekpl <tomek.barlog02@gmail.com>
2023-03-07 22:52:37 +0100

    fix creative mode tab name

--- src/main/java/net/thomasthebully/tutorialmod/item/ModCreativeModeTab.java
+++ src/main/java/net/thomasthebully/tutorialmod/item/ModCreativeModeTab.java
@@ -4,7 +4,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 
 public class ModCreativeModeTab {
-    public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorailtab") {
+    public static final CreativeModeTab TUTORIAL_TAB = new CreativeModeTab("tutorialtab") {
         @Override
         public ItemStack makeIcon() {
             return new ItemStack(Moditems.ZIRCON.get());
